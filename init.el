5f46fbc699b5710903a389907edb1462959d54eb
LakatosI <lakatos.isti@gmail.com>
2017-07-30 17:08:50 +0300

    Org mode for emacs

--- init.el
+++ init.el
@@ -10,7 +10,31 @@
 (package-initialize)
 
 (add-to-list 'load-path "~/.emacs.d/undo-tree")
+
 (add-to-list 'load-path "~/.emacs.d/goto-chg")
+
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
+
+(add-to-list 'load-path "~/.emacs.d/org-mode/lisp")
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+
+(setq org-directory "~/Dropbox/org")
+(setq org-mobile-inbox-for-pull "~/Dropbox/org/flagged.org")
+(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Dropbox/org/home.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
