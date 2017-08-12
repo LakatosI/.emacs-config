3f85c7231588fac28d994084047b54a54354910c
LakatosI <lakatos.isti@gmail.com>
2017-08-12 17:26:54 +0300

    Reset emacs config

--- init.el
+++ init.el
@@ -1,40 +1,42 @@
-(require 'package)
+;; (require 'package)
 
-(setq tab-width 4)
+;; (setq tab-width 4)
 
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+;; (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+;; (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+;; (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 
-(setq package-enable-at-startup nil)
-(package-initialize)
+;; (setq package-enable-at-startup nil)
+;; (package-initialize)
 
-(add-to-list 'load-path "~/.emacs.d/undo-tree")
+;; (add-to-list 'load-path "~/.emacs.d/undo-tree")
 
-(add-to-list 'load-path "~/.emacs.d/goto-chg")
+;; (add-to-list 'load-path "~/.emacs.d/goto-chg")
 
-(add-to-list 'load-path "~/.emacs.d/evil")
-(require 'evil)
-(evil-mode 1)
+;; (add-to-list 'load-path "~/.emacs.d/evil")
+;; (require 'evil)
+;; (evil-mode 1)
 
-(add-to-list 'load-path "~/.emacs.d/org-mode/lisp")
-(require 'org)
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
+;; (add-to-list 'load-path "~/.emacs.d/org-mode/lisp")
+;; (require 'org)
+;; (define-key global-map "\C-cl" 'org-store-link)
+;; (define-key global-map "\C-ca" 'org-agenda)
+;; (setq org-log-done t)
 
-(setq org-directory "~/Dropbox/org")
-(setq org-mobile-inbox-for-pull "~/Dropbox/org/flagged.org")
-(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/Dropbox/org/home.org"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; (setq org-directory "~/Dropbox/org")
+;; (setq org-mobile-inbox-for-pull "~/Dropbox/org/flagged.org")
+;; (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(inhibit-startup-screen t)
+;;  '(initial-buffer-choice "~/Dropbox/org/home.org")
+;;  '(org-agenda-files (quote ("~/Dropbox/org/home.org"))))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  )
