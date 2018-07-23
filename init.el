a1acafddcc68812d52a6be955a03288f5d8c4eb1
Istvan Lakatos <lakatos.isti@gmail.com>
2018-07-24 00:22:01 +0300

    Reworked myinit.org

--- init.el
+++ init.el
@@ -1,8 +1,9 @@
-
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+	     '(("org"   . "http://orgmode.org/elpa/")
+               ("gnu"   . "http://elpa.gnu.org/packages/")
+               ("melpa" . "https://melpa.org/packages/")))
 
 (package-initialize)
 
@@ -13,17 +14,3 @@
   (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (which-key use-package try solarized-theme rust-mode paredit org-bullets exec-path-from-shell evil cider auctex-latexmk ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
