212faa8d08c4a3ffb09de2befa981a8d8b10500c
Istvan Lakatos <lakatos.isti@gmail.com>
2019-04-09 22:47:51 +0300

    start work on just having a plain old init.el file

--- init.el
+++ init.el
@@ -1,9 +1,31 @@
-;; This file replaces itself with the actual configuration at first run.
-;; We can't tangle without org!
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
+			("gnu"   . "http://elpa.gnu.org/packages/")
+			("melpa" . "https://melpa.org/packages/")))
+
+(package-initialize)
+
+;; Bootstrap 'use-package'
+
+(unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
+
 (require 'org)
-;; Open the configuration
-(find-file (concat user-emacs-directory "init.org"))
-;; tangle it
-(org-babel-tangle)
-;; load it
-(load-file (concat user-emacs-directory "init.el"))
+
+(org-babel-load-file (concat user-emacs-directory "README.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (counsel ivy ox-hugo org-bullets evil-escape evil ace-window sudo-edit which-key try doom-themes exec-path-from-shell use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
