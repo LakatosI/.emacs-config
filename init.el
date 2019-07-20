ea97b477d6707f563b6b6bb7eae584a5ec6b4242
Istvan Lakatos <lakatos.isti@gmail.com>
2019-07-21 01:06:26 +0300

    Some small global key changes

--- init.el
+++ init.el
@@ -50,6 +50,73 @@
 (org-babel-load-file (ill-init-file "README.org"))
 (delete-file (ill-init-file "README.el"))
 
+(global-set-key (kbd "<C-tab>") 'complete-symbol)
+
+(global-set-key (kbd "C-x C-n") 'other-window)
+
+(defun other-window-backward (&optional n)
+  "Select the previous windew"
+  (interactive "P")
+  (other-window (- (prefix-numeric-value n))))
+
+(other-window-backward)
+
+;Need to change this to make it work with evil
+(global-set-key (kbd "C-x C-p") 'other-window-backward)
+
+
+(defalias 'scroll-ahead 'scroll-up)
+(defalias 'scroll-behind 'scroll-down)
+
+(defun scroll-one-line-ahead ()
+  "Scrolls ahead 1 line."
+  (interactive)
+  (scroll-ahead 1))
+
+(defun scroll-one-line-behind ()
+  "Scrolls ahead 1 line."
+  (interactive)
+  (scroll-behind 1))
+
+(global-set-key (kbd "C-'") 'scroll-one-line-behind)
+(global-set-key (kbd "C-;") 'scroll-one-line-behind)
+
+(defun read-only-if-symlink ()
+  "Sets buffer to read-only if file is a symlink" 
+  (if (file-symlink-p buffer-file-name)
+      (progn (setq buffer-read-only t)
+	     (message "File is a symlink!"))))
+(add-hook 'find-file-hooks 'read-only-if-symlink)
+  
+(defun visit-target-instead ()
+  "Replace the buffer with a buffer visiting the link target."
+  (interactive)
+  (if buffer-file-name
+      (let ((target (file-symlink-p buffer-file-name)))
+	(if target
+	    (find-alternate-file target)
+	  (error "Not viziting a symlink")))
+    (error "Not viziting a file")))
+
+(defun clobber-symlink ()
+  "Replace symlink with a copy of the link target"
+  (interactive)
+  (if buffer-file-name
+      (let ((target (file-symlink-p buffer-file-name)))
+	(if target
+	    (if (yes-or-no-p (format "Replace %s with %s"
+				     buffer-file-name
+				     target))
+		(progn
+		  (delete-file buffer-file-name)
+		  (write-file buffer-file-name)))
+	  (error "Not viziting a symlink")))
+    (error "Not viziting a file")))
+	      
+  
+
+
+
 (provide 'init)
 ;;; init.el ends here
 
